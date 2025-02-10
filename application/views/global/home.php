db9e9e79ce52ceb02339fd297f8f1e02f242832b
Dicky Wahyudi <wahyudidicky556@gmail.com>
2025-02-10 12:38:03 +0700

    File Upload

---
+++ application/views/global/home.php
@@ -0,0 +1,42 @@
+<div class="row">
+	<div class="col m 12">
+		<center>
+			<h5>Selamat Datang Di Sistem Informasi Perpustakaan Ushuludin Sunan Gunung Djati Bandung</h5>
+		</center><hr>
+	</div>
+
+	<div class="row">
+		<div class="col m12">
+			<div class="card-panel light-blue darken-4 center white-text">
+				<p>
+                   Sistem Informasi Perpusatakaan Ushuludin merupakan sistem yang digunakan untuk melakukan Pengolaan Data Buku yang ada di Ruang Baca Perpustakaan ushuludin.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/daftarbuku.jpg"> 
+				</div>
+				<div class="card-content light-blue darken-4 center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/list_buku">Daftar Buku</a>
+				</div>
+			</div>
+		</div>
+
+        <div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/tentang.jpg"> 
+				</div>
+				<div class="card-content light-blue darken-4 center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/profile">Tentang Perpustakaan Ushuludin</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+</div>
---
+++ application/views/global/profile.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="col m 12">
+		<h5>Perpustakaan Ushuludin UIN Sunan Gunung Djati Bandung</h5>
+	</div>
+</div>
+
+<div class="row">
+    <div class="col">
+        <div class="card-panel light-blue darken-4 white-text">
+            <h6><b>Tentang Sistem Informasi Perpustakaan Ushuludin UIN Sunan Gunung Djati Bandung</b></h6><hr>
+            <p>Secara historis, Fakultas Ushuluddin termasuk fakultas yang paling awal berdiri bersamaan dengan didirikannya UIN ( saat itu masih IAIN ) Sunan Gunung Djati Bandung, pada tahun 1968. Fakultas yang kini telah berusia 45 tahun ini, sepanjang sejarahnya telah telah dipimpin oleh figur-figur yang mumpuni di bidang keilmuan Islam.</p>
+            <p>Sistem informasi perpustakaan ini dibuat untuk memudahkan mahasiswa dan dosen mengakses daftar buku yang tersedia diperpustakaan Ushuludin UIN Sunan Gunung Djati Bandung</p>
+        </div>
+    </div>
+</div>
